cbaf03f99b62f86423fa8ebfb36e0042d22d0409
student cao107 <student@cao107.torrens>
2020-12-20 11:41:09 +1100

    Hello getout

---
+++ getout.go
@@ -0,0 +1,13 @@
+/*************************************
+*
+* Access outlook 365 email using the 
+* microsoft graph api
+*
+*************************************/
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println("Go get outlook mail.")
+}
